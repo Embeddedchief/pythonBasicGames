2bea60329fb47566be43a108ac7a897aca890d3c
embeddedchief <samuelsetemijp@gmail.com>
2026-06-03 01:56:07 +0100

    #update

--- data_types.py
+++ data_types.py
@@ -19,4 +19,11 @@ age = 15
 print(type(age))
 print(type(age) == str)
 print(isinstance(age, str))
+print("\n")
 
+title = "menu".upper()
+
+print(title.center(16, "="))
+print("Cofee".ljust(14, ".") + "$2")
+print("Tea".ljust(16, ".") + "$2")
+print("Milk".ljust(15, ".") + "$3")
